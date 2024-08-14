add1941e6f9320f36bab0fa6a48c4d13c8ddbfa6
jeisonj2019 <jeisonj2019@gmail.com>
2024-08-14 23:24:48 +0500

    package.json updated

--- app/src/index.js
+++ app/src/index.js
@@ -1,4 +1,37 @@
-import FlowJS from "../helpers/flow";
+import ElbowRect from "../resistances/ElbowRect";
+import ElbowRound from "../resistances/ElbowRound";
+
+import {
+    densityAir,
+    dynViscAir,
+    dynViscWater,
+    kinViscAir,
+    kinViscWater,
+    reinolds,
+    lambdaKoef,
+    velocityRect,
+    velocityRound,
+    hydraulicDiameter,
+    dpRoundAir,
+    dpRectAir
+} from "../helpers/flow"
+
+module.exports = {
+    ElbowRect,
+    ElbowRound,
+    densityAir,
+    dynViscAir,
+    dynViscWater,
+    kinViscAir,
+    kinViscWater,
+    reinolds,
+    lambdaKoef,
+    velocityRect,
+    velocityRound,
+    hydraulicDiameter,
+    dpRoundAir,
+    dpRectAir
+}
 
 
 
