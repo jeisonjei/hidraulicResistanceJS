a21c3a48b8a1ead6f9639ca34fc74bd7dbb059db
jeisonj2019 <jeisonj2019@gmail.com>
2024-12-31 19:06:05 +0500

    Flow class added

--- app/helpers/flow.js
+++ app/helpers/flow.js
@@ -1,44 +1,55 @@
-function densityAir(t) {
+export function densityAir(t) {
+    if (!t) return;
     return 353/(273+t);
 }
-function dynViscAir(t) {
+export function dynViscAir(t) {
     // mu
+    if (!t) return;
     return 9.80665 * (1.745 * 10 ** (-6) + 5.03 * 10 ** (-9) * t);
 }
-function dynViscWater(t) {
+export function dynViscWater(t) {
     // mu
+    if (!t) return;
     return 2.414 * 10 ** (-5) * 10 ** (247.8 / (t + 133.15));
 }
-function kinViscAir(t) {
+export function kinViscAir(t) {
     // nu
+    if (!t) return;
     var dens = densityAir(t);
     return dynViscAir(t) / dens;
 }
-function kinViscWater(t) {
+export function kinViscWater(t) {
     // nu
+    if (!t) return;
     var dens = 1000;
     return dynViscWater(t) / dens;
 }
-function reinolds(v, diameterMet, nu) {
+export function reinolds(v, diameterMet, nu) {
+    if (!v || !diameterMet || !nu) return;
     return (v * diameterMet) / nu;
 }
-function lambdaKoef(re, diameterMil) {
+export function lambdaKoef(re, diameterMil, roughness) {
+    if (!re || !diameterMil) return;
     return 0.11 * ((roughness / diameterMil) + (0.68 / re)) ** 0.25;
 }
-function velocityRect(widthMil, heightMil, flowCmh) {
+export function velocityRect(widthMil, heightMil, flowCmh) {
+    if (!widthMil || !heightMil || !flowCmh) return;
     var area = (widthMil / 1000) * (heightMil / 1000);
     var vel = (flowCmh / 3600) / area;
     return vel;
 }
-function velocityRound(diameterMil, flowCmh) {
+export function velocityRound(diameterMil, flowCmh) {
+    if (!diameterMil || !flowCmh) return;
     var area = Math.PI * (diameterMil / 2000) ** 2;
     var vel = (flowCmh / 3600) / area;
     return vel;
 }
-function hydraulicDiameter(widthMeter, heightMeter) {
+export function hydraulicDiameter(widthMeter, heightMeter) {
+    if (!widthMeter || !heightMeter) return;
     return (2 * widthMeter * heightMeter) / (widthMeter + heightMeter);
 }
-function dpRoundAir(diameterMil, flowCmh, lenMet) {
+export function dpRoundAir(diameterMil, flowCmh, lenMet) {
+    if (!diameterMil || !flowCmh || !lenMet) return;
     var diameterMet = diameterMil / 1000;
     var v = velocityRound(diameterMet, flowCmh);
     var nu = kinViscAir(t);
@@ -49,7 +60,8 @@ function dpRoundAir(diameterMil, flowCmh, lenMet) {
 
     return dp;
 }
-function dpRectAir(widthMil, heightMil, flowCmh, lenMet) {
+export function dpRectAir(widthMil, heightMil, flowCmh, lenMet) {
+    if (!widthMil || !heightMil || !flowCmh || !lenMet) return;
     var widthMet = widthMil / 1000;
     var heightMet = heightMil / 1000;
     var area = widthMet * heightMet;
@@ -72,19 +84,21 @@ function dpRectAir(widthMil, heightMil, flowCmh, lenMet) {
     return dp;
 }
 
-export {
-    densityAir,
-    dynViscAir,
-    dynViscWater,
-    kinViscAir,
-    kinViscWater,
-    reinolds,
-    lambdaKoef,
-    velocityRect,
-    velocityRound,
-    hydraulicDiameter,
-    dpRoundAir,
-    dpRectAir
+export class Flow{
+
+    static getFlowVelocity(flowRateCollectorPipeCubicMeterPerHour, areaCollectorPipeSquareMeter) {
+        var flowVelocityMeterPerSecond = (flowRateCollectorPipeCubicMeterPerHour / 3600) / areaCollectorPipeSquareMeter;
+    
+        return flowVelocityMeterPerSecond;
+    }
+
+    static getVolumeFlow(velocityPipeMeterPerSecond, areaTurnPipeSquareMeter) {
+        var volumeFlowCubicMeterPerHour = (velocityPipeMeterPerSecond * 3600) * areaTurnPipeSquareMeter;
+        return volumeFlowCubicMeterPerHour;
+    }
 }
+
+// Tests for the Flow class
+
 
 
